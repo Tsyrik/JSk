07d66119fcc74547b2ec180583e6b2c9f67feba1
Stanislav Zheludkov <stanislav.zheludkov@apriorit.com>
2022-08-11 14:22:35 +0300

    Added pascals-triangle

---
+++ pascals-triangle/pascals-triangle.js
@@ -0,0 +1,18 @@
+export const rows = (rows) => {
+
+    const arr = []
+    
+    for (let i = 0; i < rows; i++) {
+        const row = [1]
+        
+        for (let j = 1; j < i; j++) {
+            row.push(arr[i-1][j-1] + arr[i-1][j])
+        }
+        
+        if( i > 0 ) row.push(1)
+        
+        arr.push(row)
+    }
+    
+    return arr
+}
